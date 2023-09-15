c0ac7e775a5b6873f07ec81025d9c4401f88eb15
najwalahly <najwa.lahly@gmail.com>
2023-09-15 11:31:38 +0200

    add history version NOK (history showing the same move) :(

--- src/components/board.js
+++ src/components/board.js
@@ -1,79 +1,26 @@
 import './style.css'
 
 import Square from './square.js';
-import { useState } from 'react';
 
 
-export default function Board() {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-    const [status, setStatus] = useState('Next player is : X');
-
-
-    function onSquareClick(index){
-        if (calculateWinner(squares) || !squares.includes(null) || squares[index]){
-            return;
-        }
-        
-        const nextSquares = squares.slice();
-        nextSquares[index] = xIsNext ? "X" : "O";
-        setSquares(nextSquares);
-
-
-        if(!nextSquares.includes(null)){
-            setStatus("Game is over")
-            return;
-        }
-
-        const winner = calculateWinner(nextSquares);
-        if (winner){
-            setStatus('Winner is : ' + winner);
-            return;
-        }
-
-        const newPlayer = !xIsNext;
-
-        setXIsNext(newPlayer);
-        setStatus(newPlayer? 'Next player is : X' : 'Next player is : O');
-    }
-
-    function calculateWinner(squares) {
-        const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6]
-        ];
-        for (let i = 0; i < lines.length; i++) {
-          const [a, b, c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-          }
-        }
-        return null;
-      }
+export default function Board(props) { 
 
     return (
         <>
-            <div className="status">{status}</div>
             <div className="board-row">
-                <Square value={squares[0]} handleClick={() => onSquareClick(0)}/>
-                <Square value={squares[1]} handleClick={() => onSquareClick(1)}/>
-                <Square value={squares[2]} handleClick={() => onSquareClick(2)}/>
+                <Square value={props.squares[0]} handleClick={() => props.handleClick(0)}/>
+                <Square value={props.squares[1]} handleClick={() => props.handleClick(1)}/>
+                <Square value={props.squares[2]} handleClick={() => props.handleClick(2)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[3]} handleClick={() => onSquareClick(3)}/>
-                <Square value={squares[4]} handleClick={() => onSquareClick(4)}/>
-                <Square value={squares[5]} handleClick={() => onSquareClick(5)}/>
+                <Square value={props.squares[3]} handleClick={() => props.handleClick(3)}/>
+                <Square value={props.squares[4]} handleClick={() => props.handleClick(4)}/>
+                <Square value={props.squares[5]} handleClick={() => props.handleClick(5)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[6]} handleClick={() => onSquareClick(6)}/>
-                <Square value={squares[7]} handleClick={() => onSquareClick(7)}/>
-                <Square value={squares[8]} handleClick={() => onSquareClick(8)}/>
+                <Square value={props.squares[6]} handleClick={() => props.handleClick(6)}/>
+                <Square value={props.squares[7]} handleClick={() => props.handleClick(7)}/>
+                <Square value={props.squares[8]} handleClick={() => props.handleClick(8)}/>
             </div>
         </>
     )
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Board from './components/board';
+import Game from './components/game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Board />
+    <Game />
   </React.StrictMode>
 );
 
